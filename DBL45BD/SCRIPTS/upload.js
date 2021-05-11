023067dc3224e111742166b6cb82235743938ab4
MaikoCheng <71335375+MaikoCheng@users.noreply.github.com>
2021-05-11 21:55:35 +0200

    Delete upload.js

--- DBL45BD/SCRIPTS/upload.js
+++
@@ -1,5 +0,0 @@
-const realFileBtn = document.getElementById("real-file");
-const mybutton = document.getElementById("data");
-mybutton.addEventListener("click", function(){
-    realFileBtn.click();
-});
