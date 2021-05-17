1c6a29828d877ea0193d27662ef10121cae4524d
MaikoCheng <71335375+MaikoCheng@users.noreply.github.com>
2021-05-18 00:30:39 +0200

    Delete SCRIPTS directory

--- SCRIPTS/home.js
+++
@@ -1,5 +0,0 @@
-const realhome = document.getElementById("homebutton");
-const myhomebutton = document.getElementById("home");
-myhomebutton.addEventListener("click", function(){
-    realhome.click();
-});
