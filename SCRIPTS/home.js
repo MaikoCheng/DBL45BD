56f965442bc7f4ee93fd88268eaa8410800688d8
MaikoCheng <71335375+MaikoCheng@users.noreply.github.com>
2021-05-18 00:33:16 +0200

    Add files via upload

---
+++ SCRIPTS/home.js
@@ -0,0 +1,5 @@
+const realhome = document.getElementById("homebutton");
+const myhomebutton = document.getElementById("home");
+myhomebutton.addEventListener("click", function(){
+    realhome.click();
+});
