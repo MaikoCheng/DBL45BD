68197c489cf97ffe4ffd53beadd35f3a6262f427
MaikoCheng <71335375+MaikoCheng@users.noreply.github.com>
2021-05-17 23:58:29 +0200

    Add files via upload

---
+++ DBL45BD/DBL45BD/SCRIPTS/upload.js
@@ -0,0 +1,5 @@
+const realFileBtn = document.getElementById("real-file");
+const mybutton = document.getElementById("data");
+mybutton.addEventListener("click", function(){
+    realFileBtn.click();
+});
