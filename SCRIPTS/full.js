8b753fde9b532ec63d43bce0e358e499c09cacea
MaikoCheng <71335375+MaikoCheng@users.noreply.github.com>
2021-06-02 16:06:19 +0200

    Add files via upload

---
+++ SCRIPTS/full.js
@@ -0,0 +1,14 @@
+const buttonful1 = document.getElementById("fullscreen1");
+buttonful1.addEventListener("click", function(){
+  document.getElementById("my_dataviz").style.width = "91.5%";
+  document.getElementById("my_dataviz").style.height = "80%";
+  document.getElementById("my_dataviz").style.zIndex = 999;
+  document.getElementById("fullscreenclose").style.display = "block";
+  document.getElementById("fullscreenclose").style.zIndex = 1000;
+  document.getElementById("nameb").style.display = "none";
+  document.getElementById("my_dataviz2").style.display = "none";
+  document.getElementById("main_text").style.display = "none";
+  document.getElementById("main_text2").style.display = "none";
+  document.getElementById("fullscreen2").style.display = "none";
+  document.getElementById("fullscreen1").style.display = "none";
+});
