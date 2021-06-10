db69d6a27eeb04a4c33d1c20f79cdf31e9d1b23d
MaikoCheng <71335375+MaikoCheng@users.noreply.github.com>
2021-06-10 12:34:03 +0200

    Update full2.js

--- SCRIPTS/full2.js
+++ SCRIPTS/full2.js
@@ -2,7 +2,7 @@ const buttonful2 = document.getElementById("fullscreen2");
 buttonful2.addEventListener("click", function(){
 document.getElementById("fullscreenclose").style.display = "block";
 document.getElementById("my_dataviz2").style.width = "100%";
-document.getElementById("my_dataviz2").style.height = "100%";
+document.getElementById("my_dataviz2").style.height = "150%";
 document.getElementById("my_dataviz2").style.zIndex = 999;
 document.getElementById("namea").style.display = "none";
 document.getElementById("my_dataviz").style.display = "none";
