b4560c16fd09029085f5caffddf930ca9050681d
MaikoCheng <71335375+MaikoCheng@users.noreply.github.com>
2021-06-10 22:05:28 +0200

    Update fullclose.js

--- SCRIPTS/fullclose.js
+++ SCRIPTS/fullclose.js
@@ -4,6 +4,7 @@ buttonfulclose.addEventListener("click", function(){
   document.getElementById("my_dataviz2").style.width = "49.5%";
   document.getElementById("my_dataviz2").style.zIndex = 1;
   document.getElementById("my_dataviz").style.width = "49.5%";
+  document.getElementById("my_dataviz2").style.height = "100%";
   document.getElementById("my_dataviz").style.zIndex = 1;
   document.getElementById("nameb").style.display = "block";
   document.getElementById("namea").style.display = "block";
